b08833d3655a7e5bf5033321fa1a7ed52ec04779
Haris Ali <harisali.contact@gmail.com>
2025-01-14 19:00:50 +0500

    Added Layout

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -4,9 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Contact Page</title>
+    <title>Home Page</title>
 </head>
 <body>
-    <h1>Hello this is Contact page</h1>
+<nav>
+    <a href="/">Home</a>
+    <a href="about">About</a>
+    <a href="contact">Contact</a>
+</nav>
+<h1>Hello this is Home page</h1>
 </body>
 </html>
--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Contact Page</title>
+    <title>About Page</title>
 </head>
 <body>
-    <h1>Hello this is Contact page</h1>
+<h1>Hello this is About page</h1>
 </body>
 </html>
